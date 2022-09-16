80c9408e4fa3951d3ca1baa0ca4a849bf74f33fd
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-09-15 23:06:26 -0300

    movieCatalog style

--- src/pages/Protected/MovieCatalog/index.tsx
+++ src/pages/Protected/MovieCatalog/index.tsx
@@ -1,11 +1,23 @@
+import './styles.css';
 
 const MovieCatalog = () => {
-
-    return (
-        <div>
-            Catalog
-        </div>
-    );
-}
+  return (
+    <div className="movie-catalog-container">
+      <div className="movie-catalog-title">
+        <h1>Tela de listagem de filmes</h1>
+      </div>
+      <div className="movie-catalog-list">
+        <ul>
+          <li>
+            <a href="/movies/1">Acessar /movies/1</a>
+          </li>
+          <li>
+            <a href="/movies/2">Acessar /movies/2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default MovieCatalog;
--- src/pages/Protected/MovieDetails/index.tsx
+++ src/pages/Protected/MovieDetails/index.tsx
@@ -1,12 +1,20 @@
 import ReviewForm from 'components/ReviewForm';
 import ReviewListing from 'components/ReviewListing';
+import { useParams } from 'react-router-dom';
 import './styles.css';
 
+type UrlParams = {
+ movieId: string;
+};
+
 const MovieDetails = () => {
+
+  const { movieId } = useParams<UrlParams>();
+
   return (
     <div className="movie-details-container">
       <div className="movie-details-title">
-        <h1>Tela detalhes do filme id: 1 </h1>
+        <h1>Tela detalhes do filme id: {movieId} </h1>
       </div>
       <div className="movie-details-form">
           <ReviewForm />
