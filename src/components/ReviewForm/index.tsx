aae467e3c92cd6abcdb5530fd5b5d66c1158ee93
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-09-21 22:16:34 -0300

    save review

--- src/components/ReviewForm/index.tsx
+++ src/components/ReviewForm/index.tsx
@@ -1,20 +1,62 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 type Props = {
   movieId: string;
-}
+};
 
-const ReviewForm = ({movieId} : Props) => {
+type FormData = {
+  movieId: number;
+  text: string;
+};
+
+const ReviewForm = ({ movieId }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    formData.movieId = parseInt(movieId);
+
+    const params: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/reviews',
+      data: formData,
+      withCredentials: true,
+    };
+
+    requestBackend(params)
+      .then((response) => {
+        setValue('text', '');
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="form-review-container">
-      <div className="form-review-input">
-        <input className="form-control"
-          type="text" 
-          placeholder="Deixe sua avaliação aqui" />
-      </div>
-      <div className="form-review-btn">
-        <button className="btn btn-primary">SALVAR AVALIAÇÃO</button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-review-input">
+          <input
+            className="form-control"
+            {...register('text', { required: 'Campo obrigatório' })}
+            name="text"
+            type="text"
+            placeholder="Deixe sua avaliação aqui"
+          />
+          <div>{errors.text?.message}</div>
+        </div>
+        <div className="form-review-btn">
+          <button className="btn btn-primary">SALVAR AVALIAÇÃO</button>
+        </div>
+      </form>
     </div>
   );
 };
