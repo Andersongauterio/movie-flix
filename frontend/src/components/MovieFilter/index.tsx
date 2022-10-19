c1c55844fbaed3cee01e122a8d8fb5c64e45944a
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-10-18 22:17:40 -0300

    MovieFilter Select component

--- frontend/src/components/MovieFilter/index.tsx
+++ frontend/src/components/MovieFilter/index.tsx
@@ -1,9 +1,50 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+import { Genre } from 'types/genre';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
+export type GenreFilterData = {
+  genre: Genre | null;
+};
+
 const MovieFilter = () => {
+  const [selectGenres, setSelectGenres] = useState<Genre[]>([]);
+
+  const { register, handleSubmit, setValue, getValues, control } =
+		useForm<GenreFilterData>();
+
+  useEffect(() => {
+    const params: AxiosRequestConfig = {
+        method: 'GET',
+        url: '/genres',
+        withCredentials: true,
+      };
+
+    requestBackend(params).then((response) => {
+      setSelectGenres(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
-    <div  className="movie-filter-container">
-      <h1>Filter</h1>
+    <div className="movie-filter-container">
+      <Controller
+        name="genre"
+        control={control}
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={selectGenres}
+            isClearable
+            placeholder="Gênero"
+            getOptionLabel={(genre: Genre) => genre.name}
+            getOptionValue={(genre: Genre) => String(genre.id)}
+          />
+        )}
+      />
     </div>
   );
 };
